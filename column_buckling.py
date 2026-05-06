2814c36ae8949d1d46200b814122424220c3976d
ori2612-blip <ori2612@gmail.com>
2026-05-06 15:41:36 +0300

    Update column_buckling.py

--- column_buckling.py
+++ column_buckling.py
@@ -1,7 +1,10 @@
-   def column_stress_error(L, E, A, r, c, e, sigma_allow):
+def column_stress_error(P, L, E, A, r, c, e, sigma_allow):
     # פרמטרים לדוגמה
-   # A, E, L, r, e, c = 5000, 200000, 3000, 50, 20, 100
+    #A, E, L, r, e, c = 5000, 200000, 3000, 50, 20, 100
     # נוסחת הסקנט
     sec_term = 1 / np.cos((L/(2*r)) * np.sqrt(P/(E*A)))
     sigma_max = (P/A) * (1 + (e*c/r**2) * sec_term)
     return sigma_max - sigma_allow
+def find_critical_load(L, E, A, r, c, e, sigma_allow):
+  P_critical = optimize.newton(lambda P: column_stress_error(P, L, E, A, r, c, e, sigma_allow), 500000)
+  return P_critical
